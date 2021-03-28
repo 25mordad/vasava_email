3c2345d4a7a8337253d5754b7c70f243300c204b
25modad <25mordad@gmail.com>
2021-03-28 12:54:57 +0200

    send email by sendgrid api - DONE

--- backend/controllers/email.js
+++ backend/controllers/email.js
@@ -4,8 +4,24 @@ export const sendEmail = async (req,res) => {
 
 
   try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: '25mordad@gmail.com',
+      from: 'aalinoosh@gmail.com',
+      subject: 'Email : ' ,
+      html: 'Hi',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent');
+        res.status(200).json({"send":"true"});
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(404).json({ message: error.message });
+      })
 
-    console.log("email sent")
 
   } catch (e) {
     res.status(404).json({ message: error.message });
